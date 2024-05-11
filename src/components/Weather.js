73f9f6f0fe9d2a5be2fc0f4c64588114467114c1
unknown <negi60239@gmail.com>
2024-05-11 13:29:26 +0530

    task13

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./weather.css";
 
@@ -7,71 +7,51 @@ const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const [debounceTimer, setDebounceTimer] = useState(null);
 
-  const apiKey = "YOUR_API_KEY";
+  const apiKey = "37454a6382fc47caab2111239240605";
 
   useEffect(() => {
-    if (city.trim() === "") {
-      setWeatherData(null);
-      setError("Please enter a city name");
-      return;
-    }
-
-    const fetchData = async () => {
-      setLoading(true);
-      setError("");
-
-      try {
-        const response = await axios.get(
-          `https://api.weatherapi.com/v1/current.json?q=${city}&key=${apiKey}`
-        );
-        const data = response.data;
-        setWeatherData(data);
-      } catch (error) {
-        setError("Error fetching data. Please try again later.");
-        setWeatherData(null);
+    const debounceTimeout = setTimeout(() => {
+      if (city.trim() === "") {
+        setWeatherData(null)
+        setError("please enter the city name")
+            
+      
       }
-
-      setLoading(false);
-    };
-
-    if (debounceTimer) {
-      clearTimeout(debounceTimer);
-    }
-
-    const timer = setTimeout(() => {
-      fetchData();
+      
     }, 1000);
 
-    setDebounceTimer(timer);
+    return () => clearTimeout(debounceTimeout);
+  }, [city]);
 
-    return () => clearTimeout(timer);
-  }, [city, apiKey, debounceTimer]);
+  const getWeather = async () => {
+    setLoading(true);
+    setError("");
+ 
+    
+    try {
+      const response = await axios.get(
+        `https://api.weatherapi.com/v1/current.json?q=${city}&key=${apiKey}`
+      );
+      const data = await response.data;
+      setWeatherData(data);
+    } catch (error) {
+      setError("Error fetching data. Please try again later.");
+      setWeatherData(null)
+    }
+
+    setLoading(false);
+  };
 
   const handleSearch = () => {
     if (city.trim() === "") {
-      setError("Please enter a city name.");
+      // setError("Please enter the city name.");
       return;
+
+
     }
-
-    setDebounceTimer(null); // Clear any existing debounce timer
-    setLoading(true);
-    setError("");
-
-    axios
-      .get(`https://api.weatherapi.com/v1/current.json?q=${city}&key=${apiKey}`)
-      .then((response) => {
-        const data = response.data;
-        setWeatherData(data);
-      })
-      .catch((error) => {
-        setError("Error fetching data. Please try again later.");
-        setWeatherData(null);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    getWeather();
+    
   };
 
   const handleChange = (e) => {
@@ -95,21 +75,15 @@ const Weather = () => {
       {loading && <p className="loading-msg">Loading data...</p>}
       {error && <p className="error-msg">{error}</p>}
       <div className="weather-cards">
+
+      
         {weatherData && (
           <div className="weather-card">
-            <p>
-              Temperature: <strong>{weatherData.current.temp_c}°C</strong>
-            </p>
-            <p>
-              Humidity: <strong>{weatherData.current.humidity}%</strong>
-            </p>
-            <p>
-              Condition: <strong>{weatherData.current.condition.text}</strong>
-            </p>
-            <p>
-              Wind Speed:{" "}
-              <strong>{weatherData.current.wind_kph} km/h</strong>
-            </p>
+               
+            <p>Temperature: <strong>{weatherData.current.temp_c}°C</strong> </p>
+            <p>Humidity: <strong>{weatherData.current.humidity}%</strong></p>
+            <p>Condition: <strong>{weatherData.current.condition.text}</strong></p>
+            <p>Wind Speed: <strong>{weatherData.current.wind_kph} km/h</strong></p>
           </div>
         )}
       </div>
